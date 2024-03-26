12330eb33e249bbc6c1b7ee7a67bb210cabf4b60
NuomiBear <fakesola@163.com>
2024-03-27 02:32:00 +0800

    0326night_super

--- src/Class.js
+++ src/Class.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client';
 
 class Class extends React.Component {
     constructor(props) {
-        console.log(props)
-        super(props)
+        console.log('props',props)
+        // 调用父类的构造函数并传递props 以便继承父类的属性和方法
+        // super(props)
+        this.classA = 'classA'
     }
 
     render() {
