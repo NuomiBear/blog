b0c9e5675126527496f8e937e44388465ab78b56
NuomiBear <fakesola@163.com>
2024-05-16 02:32:07 +0800

    0516

--- src/App.js
+++ src/App.js
@@ -17,29 +17,55 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          This is first version 0.0.1
-        </p>
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {/* Learn React */}
-        </a>
-        <AppClass />
-      </header>
+      <div className='content'>
+        {/* PROFILE */}
+        <header className="header">
+          <div className='user'>
+            <a href='/' className='link_user'>
+              <div className='userinfo'>
+                <span className='avatar'>
+                  <img src="/avatar.png"></img>
+                </span>
+                <div className='names'>
+                  <span className='name'>EKB</span>
+                  <span className='info'>JavaScript 工程师，学徒。</span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className='link'>
+            <a href='/blog'>所有文章</a>
+            <a href='/about'>关于我</a>
+            <a href='/others'>其他</a>
+          </div>
+        </header>
+        {/* NOTES */}
+        <div className='dynamic-content'>
+          <section>
+            <h2>最近</h2>
+            <div className='content'>
+              <div className='jsx_1'>
+                <a>文章1
+                  <span>2024年5月16日</span>
+                </a>
+              </div>
+              <div className='jsx_2'>
+                <a>文章2
+                  <span>2024年5月16日</span>
+                </a>
+              </div>
+              <div className='jsx_3'>
+                <a>文章3
+                  <span>2024年5月16日</span>
+                </a>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
